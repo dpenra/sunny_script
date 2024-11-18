cac603725106ca1f164dc4316933ebc231043039
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-11-18 14:05:53 +0545

    Update rising15.sh

--- rising15.sh
+++ rising15.sh
@@ -13,3 +13,10 @@ repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
 # -j$(nproc --all): Use all available CPU cores
 # repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 /opt/crave/resync.sh
+rm -rf packages/apps/ViPER4AndroidFX
+git clone https://github.com/xiaomi-begonia-dev/android_packages_apps_ViPER4AndroidFX.git --depth 1 -b fourteen packages/apps/ViPER4AndroidFX
+rm -rf vendor/bcr
+git clone https://github.com/Chaitanyakm/vendor_bcr.git --depth 1 -b main vendor/bcr
+. build/envsetup.sh
+riseup sunny user
+rise b
