a1c38697c2c1d7c507ed9561f079cd9cf72ba680
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-11-18 21:53:41 +0545

    Update CORE_sunny.sh

--- CORE_sunny.sh
+++ CORE_sunny.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# Initialize the ROM manifest for RisingTech on branch 'fifteen' with Git LFS enabled
-# repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
 repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
-
-# Sync the repository with various options for efficient and clean syncing:
-# -c: Current branch only
-# --no-clone-bundle: Skip clone bundle usage
-# --optimized-fetch: Perform optimized fetch for faster syncing
-# --prune: Remove obsolete branches
-# --force-sync: Ensure a full sync of all repositories
-# -j$(nproc --all): Use all available CPU cores
-# repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 /opt/crave/resync.sh
 
 rm -rf packages/apps/ViPER4AndroidFX
