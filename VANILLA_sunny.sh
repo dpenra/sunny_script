248d7d8004b05ac4b1c1dcd05280fa2462f50225
dpenra <dpenra.stha@gmail.com>
2024-11-18 15:04:34 +0545

    update

--- VANILLA_sunny.sh
+++ VANILLA_sunny.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Initialize the ROM manifest for RisingTech on branch 'fifteen' with Git LFS enabled
+# repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
 repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
-/opt/crave/resync.sh
+
+# Sync the repository with various options for efficient and clean syncing:
+# -c: Current branch only
+# --no-clone-bundle: Skip clone bundle usage
+# --optimized-fetch: Perform optimized fetch for faster syncing
+# --prune: Remove obsolete branches
+# --force-sync: Ensure a full sync of all repositories
+# -j$(nproc --all): Use all available CPU cores
+# repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+# /opt/crave/resync.sh
+
 rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/xiaomi-begonia-dev/android_packages_apps_ViPER4AndroidFX.git --depth 1 -b fourteen packages/apps/ViPER4AndroidFX
 rm -rf vendor/bcr
