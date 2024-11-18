31618897831e81013c1f9e246748d8326db6f115
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-11-19 00:07:14 +0545

    Update VANILLA_sunny.sh

--- VANILLA_sunny.sh
+++ VANILLA_sunny.sh
@@ -12,14 +12,14 @@ repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
 # --force-sync: Ensure a full sync of all repositories
 # -j$(nproc --all): Use all available CPU cores
 # repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-# /opt/crave/resync.sh
+/opt/crave/resync.sh
 
 rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/xiaomi-begonia-dev/android_packages_apps_ViPER4AndroidFX.git --depth 1 -b fourteen packages/apps/ViPER4AndroidFX
 rm -rf vendor/bcr
 git clone https://github.com/Chaitanyakm/vendor_bcr.git --depth 1 -b main vendor/bcr
 . build/envsetup.sh
-export OUT_DIR=out2
+# export OUT_DIR=out2
 export BUILD_USERNAME=dpenra
 export BUILD_HOSTNAME=crave
 export RISING_PACKAGE_TYPE="VANILLA_AOSP"
