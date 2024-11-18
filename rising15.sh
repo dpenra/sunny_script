fcb9ad9f2524147d213409f28025b11761412bc4
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-11-18 14:12:12 +0545

    Update rising15.sh

--- rising15.sh
+++ rising15.sh
@@ -7,5 +7,23 @@ git clone https://github.com/xiaomi-begonia-dev/android_packages_apps_ViPER4Andr
 rm -rf vendor/bcr
 git clone https://github.com/Chaitanyakm/vendor_bcr.git --depth 1 -b main vendor/bcr
 . build/envsetup.sh
+export BUILD_USERNAME=dpenra
+export BUILD_HOSTNAME=crave
+export TARGET_DEFAULT_PIXEL_LAUNCHER=true
+export TARGET_ENABLE_PIXEL_FEATURES=true
+export TARGET_PREBUILT_LAWNCHAIR_LAUNCHER=true
+export TARGET_CORE_GMS=true
+# export TARGET_DISABLE_EPPE=true
+# export TARGET_USE_MIUI_SCANNER=true
+export WITH_GMS=true
+export PRODUCT_PACKAGES+=(
+    MarkupGoogle
+    LatinIMEGooglePrebuilt
+    AiWallpapers
+    WallpaperEmojiPrebuilt
+    PrebuiltDeskClockGoogle
+    CalculatorGooglePrebuilt
+    CalendarGooglePrebuilt
+)
 riseup sunny user
 rise b
